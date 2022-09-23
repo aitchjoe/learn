45e6c1837db226ae5efff2135136cccc54185746
aitchjoe <aitchjoe@outlook.com>
2022-09-23 21:03:42 +0800

    fuzz test and bug found

--- README.md
+++ README.md
@@ -3,3 +3,26 @@
 ## Test
 
 * `go test -v ./pkg/shift`
+
+## Fuzz
+
+* `go test -fuzz=Fuzz ./pkg/shift`
+
+Bug found:
+```
+C:\joe\workspace\aitchjoe\learn>go test -fuzz=Fuzz ./pkg/shift
+fuzz: elapsed: 0s, gathering baseline coverage: 0/2 completed
+fuzz: elapsed: 0s, gathering baseline coverage: 2/2 completed, now fuzzing with 16 workers
+fuzz: minimizing 49-byte failing input file
+fuzz: elapsed: 0s, minimizing
+--- FAIL: FuzzShift (0.08s)
+    --- FAIL: FuzzShift (0.00s)
+        shift_test.go:93: Shift(Shift("0", []), []) = "", want "0"
+
+    Failing input written to testdata\fuzz\FuzzShift\8d9227f8196001c8766ef2332f636f0aeb3015755c7c13d9163781ca4f78b2b6
+    To re-run:
+    go test -run=FuzzShift/8d9227f8196001c8766ef2332f636f0aeb3015755c7c13d9163781ca4f78b2b6
+FAIL
+exit status 1
+FAIL    github.com/aitchjoe/learn/pkg/shift     0.276s
+```
--- pkg/shift/shift_test.go
+++ pkg/shift/shift_test.go
@@ -1,7 +1,9 @@
 package shift
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 )
 
 // Ref: https://blog.csdn.net/keephide/article/details/108968360
@@ -44,4 +46,51 @@ func TestShiftOverflow(t *testing.T) {
 	if result != want {
 		t.Fatalf(`Shift("%q, %v") = %q, want %q`, s, shift, result, want)
 	}
+}
+
+func reverseShift(shift [][2]int) [][2]int {
+	rev := [][2]int{}
+	for i := len(shift) - 1; i >= 0; i-- {
+		sh := shift[i]
+		var d int
+		if sh[0] == 0 {
+			d = 1
+		} else {
+			d = 0
+		}
+		rev = append(rev, [2]int{d, sh[1]})
+	}
+	return rev
+}
+
+func TestReverseShift(t *testing.T) {
+	s := "abcdefg"
+	shift := [][2]int{{1, 1}, {1, 1}, {0, 2}, {1, 3}}
+	result := Shift(s, shift)
+	rev := reverseShift(shift)
+	r := Shift(result, rev)
+	if r != s {
+		t.Fatalf(`Shift(Shift(%q, %v), %v) = %q, want %q`, s, shift, rev, r, s)
+	}
+}
+
+func FuzzShift(f *testing.F) {
+	rand.Seed(time.Now().UnixNano())
+	f.Add("abcdefg", uint8(10))
+	f.Add("万有引力", uint8(5))
+	f.Fuzz(func(t *testing.T, s string, steps uint8) {
+		steps %= 20
+		shift := [][2]int{}
+		for i := uint8(0); i < steps; i++ {
+			d := int(rand.Uint32() % 2)
+			a := int(rand.Uint32() % 30)
+			shift = append(shift, [2]int{d, a})
+		}
+		result := Shift(s, shift)
+		rev := reverseShift(shift)
+		r := Shift(result, rev)
+		if r != s {
+			t.Errorf(`Shift(Shift(%q, %v), %v) = %q, want %q`, s, shift, rev, r, s)
+		}
+	})
 }
